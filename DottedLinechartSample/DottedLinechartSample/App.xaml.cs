52a99cb51a0c452fd5e83d15f2f74afd4a35b6f5
Harsha <harshamidadhala.k@syncfusion.com>
2025-04-28 17:08:52 +0530

    version updated

--- DottedLinechartSample/DottedLinechartSample/App.xaml.cs
+++ DottedLinechartSample/DottedLinechartSample/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace DottedLinechartSample;
-
-public partial class App : Application
+﻿namespace DottedLinechartSample
 {
-	public App()
-	{
-		InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
 
-		MainPage = new AppShell();
-	}
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
 }
